26a6b78177feb866228266e7670601c073c7cd3f
Josh-Muleshi <jmuleshi2@gmail.com>
2022-07-19 16:36:15 +0200

    all done

--- app/src/main/java/cd/wayupdev/project_mobile/AjouterTravailActivity.java
+++ app/src/main/java/cd/wayupdev/project_mobile/AjouterTravailActivity.java
@@ -2,12 +2,14 @@ package cd.wayupdev.project_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AjouterTravailActivity extends AppCompatActivity {
 
@@ -25,6 +27,7 @@ public class AjouterTravailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_travail);
         initialiserComposant();
+        checkFieldConfirmation();
     }
 
     private void initialiserComposant(){
@@ -36,5 +39,49 @@ public class AjouterTravailActivity extends AppCompatActivity {
         buttonAjouterTravail = findViewById(R.id.btnAjoutModifier);
     }
 
+    private void checkFieldConfirmation(){
+        buttonAjouterTravail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = editTextDescription.getText().toString();
+                String promotion = editTextPromotion.getText().toString();
+                String category = editTextCategory.getText().toString();
+                String date = editTextDateTravail.getText().toString();
 
+                if (description.isEmpty()){
+                    Toast.makeText(
+                            AjouterTravailActivity.this,
+                            "Le champ description ne doit pas etre vide",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }else if (promotion.isEmpty()){
+                    Toast.makeText(
+                            AjouterTravailActivity.this,
+                            "Le champ promotion ne doit pas etre vide",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }else  if (category.isEmpty()){
+                    Toast.makeText(
+                            AjouterTravailActivity.this,
+                            "Le champ category ne doit pas etre vide",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }else if (date.isEmpty()){
+                    Toast.makeText(
+                            AjouterTravailActivity.this,
+                            "Le champ date ne doit pas etre vide",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }else {
+                    goToHomePage();
+                }
+
+            }
+        });
+    }
+
+    private void goToHomePage(){
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
+    }
 }
