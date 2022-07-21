019317e12b40c7f098a93c0c3cb59706c3c44829
Josh-Muleshi <jmuleshi2@gmail.com>
2022-07-21 11:04:39 +0200

    add the modifier Profile Activity

---
+++ app/src/main/java/cd/wayupdev/project_mobile/ModifierProfileActivity.java
@@ -0,0 +1,14 @@
+package cd.wayupdev.project_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifierProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifier_profile);
+    }
+}
--- app/src/main/java/cd/wayupdev/project_mobile/ProfileActivity.java
+++ app/src/main/java/cd/wayupdev/project_mobile/ProfileActivity.java
@@ -1,8 +1,13 @@
 package cd.wayupdev.project_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_edit) {
+            Intent intent = new Intent(ProfileActivity.this, ModifierProfileActivity.class);
+            startActivity(intent);
+        }else
+            Toast.makeText(this, "Edit menu clicked", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
